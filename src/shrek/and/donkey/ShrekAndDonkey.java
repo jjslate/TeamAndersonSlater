3ca154429ffbd4f14ffbb45c47c41e1c2c038a7c
Jayson <Jayson@10.18.149.26>
2015-01-28 09:58:01 -0700

    initial commit of project

---
+++ src/shrek/and/donkey/ShrekAndDonkey.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shrek.and.donkey;
+
+/**
+ *
+ * @author Jayson
+ */
+public class ShrekAndDonkey {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
