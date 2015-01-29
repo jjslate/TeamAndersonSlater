710f6b145f9d3a0a2c7f12be2e43658ec4c8e517
Jayson <Jayson@10.18.149.249>
2015-01-29 09:35:00 -0700

    first team commit

--- src/CandJ/shrekanddonkey/model/Player.java
+++ src/CandJ/shrekanddonkey/model/Player.java
@@ -5,10 +5,70 @@
  */
 package CandJ.shrekanddonkey.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
+    
 /**
  *
  * @author Jayson
  */
-public class Player {
+public class Player implements Serializable{
+    
+     // class instance variables
+    private String name;
+    private double highScore;
+
+    public Player() {
+    }
+    
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHighScore() {
+        return highScore;
+    }
+
+    public void setHighScore(double highScore) {
+        this.highScore = highScore;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", highScore=" + highScore + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.name);
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.highScore) ^ (Double.doubleToLongBits(this.highScore) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.highScore) != Double.doubleToLongBits(other.highScore)) {
+            return false;
+        }
+        return true;
+    }
     
 }
--- src/shrek/and/donkey/ShrekAndDonkey.java
+++ src/shrek/and/donkey/ShrekAndDonkey.java
@@ -5,17 +5,28 @@
  */
 package shrek.and.donkey;
 
+import CandJ.shrekanddonkey.model.Player;
+
 /**
  *
  * @author Jayson
  */
 public class ShrekAndDonkey {
-
+    
+   
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Player playerOne = new Player();
+        
+        playerOne.setName("Fred Flintsone");
+        playerOne.setHighScore(7.00);
+        
+        String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
+        
     }
     
 }
