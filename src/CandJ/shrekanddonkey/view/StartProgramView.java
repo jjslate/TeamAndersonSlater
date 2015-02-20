5a0a9bd4f49ba914364a9afd9b25409c333adbab
Owner <Owner@10.18.151.97>
2015-02-20 10:00:55 -0700

    Lesson 6 team assignment

--- src/CandJ/shrekanddonkey/view/StartProgramView.java
+++ src/CandJ/shrekanddonkey/view/StartProgramView.java
@@ -26,23 +26,23 @@ public class StartProgramView {
     }
 
     public void displayBanner() {
-       System.out.println("\n\n*********************************************");
+   System.out.println("\n\n*************************************************");
        
        System.out.println("*                                               *" 
-                        + "\n* This is the Donkey and Shrek game           *"
-                        + "\n* In this game you will take the role of      *"
-                        + "\n* Donkey. Donkey's friend Shrek has a problem *"
-                        + "\n* with fairytale creatures in his swamp. You  *"
-                        + "\n* help him on his journey to find the princess*"
-                        + "\n* so that the creatures will be removed from  *"
-                        + "\n* the swamp and Shrek can have peace and      *"
-                        + "\n* quiet. Throughout the game you must keep    *"
-                        + "\n* Shrek either happy or angry. Angry to get   *"
-                        + "\n* through obstacles and happy to continue the *"
-                        + "\n* quest.                                      *");
+                      + "\n* This is the Donkey and Shrek game             *"
+                      + "\n* In this game you will take the role of        *"
+                      + "\n* Donkey. Donkey's friend Shrek has a problem   *"
+                      + "\n* with fairytale creatures in his swamp. You    *"
+                      + "\n* help him on his journey to find the princess  *"
+                      + "\n* so that the creatures will be removed from    *"
+                      + "\n* the swamp and Shrek can have peace and        *"
+                      + "\n* quiet. Throughout the game you must keep      *"
+                      + "\n* Shrek either happy or angry. Angry to get     *"
+                      + "\n* through obstacles and happy to continue the   *"
+                      + "\n* quest.                                        *");
        System.out.println("*                                               *"
-                        +"\n* Good luck and may the odds be ever in your   *"
-                        +"\n* favor.                                       *");
+                       +"\n* Good luck and may the odds be ever in your    *"
+                       +"\n* favor.                                        *");
        System.out.println("*************************************************");
      }
 
